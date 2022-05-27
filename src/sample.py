25a0cf927d0b394bc68088cd44ba00ee502c3bcc
tok41 <hippy.hikky@gmail.com>
2022-05-27 22:34:03 +0900

    update settings.json

--- src/sample.py
+++ src/sample.py
@@ -2,11 +2,22 @@
 """
 
 
-def hoge(x):
+from typing import Union
+
+
+def hoge(x: float) -> Union[int, float]:
+    """sample function
+
+    Args:
+        x (float): input value
+
+    Returns:
+        Union[int, float]: return value -> numeric value
+    """
     return x
 
 
 if __name__ == "__main__":
-    a = 100
+    a: float = 100.0
     z = hoge(a)
     print(f"{z}")
